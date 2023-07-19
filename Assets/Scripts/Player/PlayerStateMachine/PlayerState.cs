dd3cb6cd99cfa6614a2158ce7c0a9e3227b3d025
SomgBird <dlarionov51@gmail.com>
2023-07-19 21:37:49 +0300

    Player state machine init

---
+++ Assets/Scripts/Player/PlayerStateMachine/PlayerState.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerState
+{
+    protected Player player;
+
+    protected PlayerStateMachine stateMachine;
+
+    protected PlayerData playerData;
+
+    public PlayerState(Player player, PlayerStateMachine stateMachine, PlayerData playerData)
+    {
+        this.player = player;
+        this.stateMachine = stateMachine;
+        this.playerData = playerData;
+    }
+    
+    public virtual void Enter()
+    {
+        
+    }
+
+    public virtual void Exit()
+    {
+        
+    }
+    
+    public virtual void LogicUpdate()
+    {
+        
+    }
+    
+    public virtual void PhysicsUpdate()
+    {
+        
+    }
+}
