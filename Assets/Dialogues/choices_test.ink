6daee703f655e8023c1abba00d14ab01bb2e4342
SomgBird <dlarionov51@gmail.com>
2023-08-04 01:59:24 +0300

    Dialogue choices

---
+++ Assets/Dialogues/choices_test.ink
@@ -0,0 +1,12 @@
+-> main
+
+=== main ===
+Choose something!
+    +[Choice1]
+        -> chosen("1")
+    +[Choice2]
+        -> chosen("2")
+        
+=== chosen(ch) ===
+You chose {ch}!
+-> END
--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using Ink.Runtime;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.Serialization;
 
 public class DialogueManager : Singleton<DialogueManager>
@@ -11,6 +12,11 @@ public class DialogueManager : Singleton<DialogueManager>
     [Header("Dialogue UI")] 
     [SerializeField] private GameObject dialoguePanel;
     [SerializeField] private TextMeshProUGUI dialogueText;
+
+    [Header("Choices UI")] 
+    [SerializeField] private GameObject[] choices;
+    
+    private TextMeshProUGUI[] choicesText;
 
     private Story currentStory;
 
@@ -21,6 +27,10 @@ public class DialogueManager : Singleton<DialogueManager>
     {
         DialogueIsPlaying = false;
         dialoguePanel.SetActive(false);
+        
+        choicesText = new TextMeshProUGUI[choices.Length];
+        for (int i = 0; i < choices.Length; i++)
+            choicesText[i] = choices[i].GetComponentInChildren<TextMeshProUGUI>();
     }
 
     private void Update()
@@ -45,19 +55,62 @@ public class DialogueManager : Singleton<DialogueManager>
         dialoguePanel.SetActive(true);
         ContinueStory();
     }
-
-    private void ContinueStory()
-    {
-        if (currentStory.canContinue)
-            dialogueText.text = currentStory.Continue();
-        else
-            ExitDialogueMode();
-    }
-
+    
     private void ExitDialogueMode()
     {
         DialogueIsPlaying = false;
         dialoguePanel.SetActive(false);
         dialogueText.text = "";
+    }
+
+    private void ContinueStory()
+    {
+        if (currentStory.canContinue)
+        {
+            dialogueText.text = currentStory.Continue();
+            DisplayChoices();
+        }
+        else
+        {
+            ExitDialogueMode();
+        }
+    }
+    
+    // TODO: rework for any number of choices
+    public void DisplayChoices()
+    {
+        List<Choice> currentChoices = currentStory.currentChoices;
+
+        if (currentChoices.Count > choices.Length)
+        {
+            Debug.LogError("Too many choices!");
+        }
+
+        int index ;
+        for (index = 0; index < currentChoices.Count; index++)
+        {
+            choices[index].gameObject.SetActive(true);
+            choicesText[index].text = currentChoices[index].text;
+        }
+
+        for (; index < choices.Length; index++)
+        {
+            choices[index].gameObject.SetActive(false);
+        }
+
+        StartCoroutine(SelectFirstChoice());
+    }
+
+    private IEnumerator SelectFirstChoice()
+    {
+        // Clear event system and set default choice
+        EventSystem.current.SetSelectedGameObject(null);
+        yield return new WaitForEndOfFrame();
+        EventSystem.current.SetSelectedGameObject(choices[0].gameObject);
+    }
+
+    public void MakeChoice(int choiceIndex)
+    {
+        currentStory.ChooseChoiceIndex(choiceIndex);
     }
 }
