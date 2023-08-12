d41820ad947e010ad564cb07510117649fa5c95c
SomgBird <dlarionov51@gmail.com>
2023-08-12 22:47:32 +0300

    Interaction positions gizmos fix

--- Assets/Scripts/Interactive/EventTriggers/PlayerClickInteraction.cs
+++ Assets/Scripts/Interactive/EventTriggers/PlayerClickInteraction.cs
@@ -11,4 +11,13 @@ public class PlayerClickInteraction : InteractiveEventTrigger
     {
         RaiseEvent();
     }
+    
+    private void OnDrawGizmos()
+    {
+        Vector3 from = InteractionPosition;
+        Vector3 to = new(from.x, from.y + 1.5f, from.z);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(from, to);
+        Gizmos.DrawWireSphere(from, 0.3f);
+    }
 }
